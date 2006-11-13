02a4e32e5216e6c7b1ae5a90df99861c0000eccc
Ricardo SIGNES <rjbs@codesimply.com>
2006-11-13 17:00:00 +0000

    r27888@minion101:  rjbs | 2006-11-13 10:00:37 -0500  cruft

--- lib/Tree/File.pm
+++ lib/Tree/File.pm
@@ -12,13 +12,13 @@ Tree::File - store a data structure in a file tree
 
 =head1 VERSION
 
-version 0.09
+version 0.110
 
  $Id$
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.110';
 
 =head1 SYNOPSIS
 
